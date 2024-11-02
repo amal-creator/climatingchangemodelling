16c6ea7b595d281a6e28dff201d0997c5acd7839
Amalrag <amalragpop@gmail.com>
2024-11-03 00:22:04 +0530

    data validation completed

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 from climatingchangemodelling.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from climatingchangemodelling.pipeline.stage_02_data_validation import DataValidationTrainingPipeline   
 from climatingchangemodelling.logging import logger
+
 
 STAGE_NAME = 'Data Ingestion Stage'
 
 try:
     logger.info(f'{STAGE_NAME} started')
-    pipeline = DataIngestionTrainingPipeline()
-    pipeline.main()
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f'{STAGE_NAME} completed successfully')
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = 'Data validation Stage'
+
+try:
+    logger.info(f'{STAGE_NAME} started')
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
     logger.info(f'{STAGE_NAME} completed successfully')
 except Exception as e:
     logger.exception(e)
--- src/climatingchangemodelling/entity/__init__.py
+++ src/climatingchangemodelling/entity/__init__.py
@@ -7,3 +7,11 @@ class DataIngestionConfig:
     source_url: str
     local_data_files: Path
     unzip_dir: Path
+
+
+
+@dataclass(frozen=True)
+class DataValidationConfig:
+    root_dir : Path
+    STATUS_FILE : Path
+    ALL_REQUIRED_FILES : list
